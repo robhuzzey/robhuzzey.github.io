79f5057a90f60f8bc9ceaa5744f32349cfdc7bbe
Rob Huzzey <robert.huzzey@holidayextras.com>
2017-08-07 20:43:40 +0100

    deploy commit

---
+++ _next/bbc844d9-aa2b-4be5-8a22-2a887b7a87b3/page/songs/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/songs', function() {
+            var comp = module.exports=webpackJsonp([6],{432:function(e,t,l){e.exports=l(433)},433:function(e,t,l){"use strict";function o(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var r=l(0),u=o(r),a=l(87),s=o(a),n=l(48);t.default=function(){return u.default.createElement(s.default,{title:"Songs created by Rob Huzzey"},u.default.createElement(n.Grid,null,u.default.createElement(n.Row,null,u.default.createElement(n.Col,{xs:12,md:12},u.default.createElement("iframe",{width:"100%",height:"450",scrolling:"no",frameborder:"no",src:"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/20829371&color=ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false"})))))}}},[432]);
+            return { page: comp.default }
+          })
+        
