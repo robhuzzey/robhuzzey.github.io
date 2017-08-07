de4cb98a0409f9cac51e8c5ae67a40fa066ae5ea
Rob Huzzey <robert.huzzey@holidayextras.com>
2017-08-07 10:41:25 +0100

    Fetching books on build

---
+++ _next/a63cda13-ba8b-42ce-8329-0a63276a7d51/page/songs/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/songs', function() {
+            var comp = module.exports=webpackJsonp([6],{432:function(e,t,l){e.exports=l(433)},433:function(e,t,l){"use strict";function u(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var o=l(0),r=u(o),a=l(87),n=u(a),s=l(48);t.default=function(){return r.default.createElement(n.default,null,r.default.createElement(s.Grid,null,r.default.createElement(s.Row,null,r.default.createElement(s.Col,{xs:12,md:12},r.default.createElement("iframe",{width:"100%",height:"450",scrolling:"no",frameborder:"no",src:"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/20829371&color=ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false"})))))}}},[432]);
+            return { page: comp.default }
+          })
+        
